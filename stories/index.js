ccb67980f39d93f2ab9c1e400fcf561eadc6d6c4
Emily <emily.hillier@gmail.com>
2019-10-28 13:15:37 -0700

    added classNames to update css

--- stories/index.js
+++ stories/index.js
@@ -6,6 +6,7 @@ import { action } from "@storybook/addon-actions";
 import "index.scss";
 
 import Button from "components/Button";
+import DayListItem from "components/DayListItem.js";
 
 storiesOf("Button", module)
   .addParameters({
@@ -16,7 +17,7 @@ storiesOf("Button", module)
   .add("Danger", () => <Button danger>Cancel</Button>)
   .add("Clickable", () => (
     <Button onClick={action("button-clicked")}>Clickable</Button>
-    
+
   ))
   .add("Disabled", () => (
     <Button disabled onClick={action("button-clicked")}>
@@ -24,3 +25,14 @@ storiesOf("Button", module)
     </Button>
   ));
 
+
+  storiesOf("DayListItem", module)
+  .addParameters({
+    backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
+  })
+  .add("Unselected", () => <DayListItem name="Monday" spots={5} />)
+  .add("Selected", () => <DayListItem name="Monday" spots={5} selected />) 
+  .add("Full", () => <DayListItem name="Monday" spots={0} />) 
+  .add("Clickable", () => (
+    <DayListItem name="Tuesday" setDay={action("setDay")} spots={5} /> 
+  ));
