326cda55ff3c40ee6ca9a80c0122cac7dadb2c6c
Emily <emily.hillier@gmail.com>
2019-10-29 16:10:57 -0700

    added selector and tests

---
+++ src/helpers/selectors.js
@@ -0,0 +1,12 @@
+export function getAppointmentsForDay(state, day) {
+  let filteredApps = [];
+  for (let days of state.days) {
+    if (days.name === day) {
+      filteredApps = days.appointments.map(
+        appointment => state.appointments[appointment]
+      );
+    }
+  }
+  return filteredApps;
+};
+
