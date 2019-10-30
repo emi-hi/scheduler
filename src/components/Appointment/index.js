d46ff53c2d53d034a3abc6afe0a785afe12bff53
Emily <emily.hillier@gmail.com>
2019-10-30 14:26:43 -0700

    added create mode transition when button is clicked on empty time

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -2,14 +2,33 @@ import React from "react";
 import './styles.scss';
 import Show from './Show.js'
 import Empty from './Empty.js'
+import useVisualMode from "../../hooks/useVisualMode";
+import Form from './Form.js';
+
+
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const CREATE = "CREATE"
 
 export default function Appointment(props) {
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
   return (
   <article className="appointment">
     <header>{props.time}</header>
-    {props.interview? <Show
-          student={props.interview.student}
-          interviewer={props.interview.interviewer}/>: <Empty />}
+    {mode === EMPTY && <Empty onAdd={()=> {transition(CREATE)}} />}
+    {mode === SHOW && (
+    <Show
+    student={props.interview.student}
+    interviewer={props.interview.interviewer}
+    />
+    )}
+    {mode === CREATE && (
+    <Form
+    interviewers={[]}
+    onCancel={()=>{transition(EMPTY)}} />
+    )}
   </article>
   
   )
