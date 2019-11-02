c955c03483fa5d4926a2729258b45fd78970ee18
Emily <emily.hillier@gmail.com>
2019-11-02 09:26:54 -0700

    refactored

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,22 +1,22 @@
 import React from "react";
-import './styles.scss';
-import Show from './Show.js'
-import Empty from './Empty.js'
+import "./styles.scss";
+import Show from "./Show.js";
+import Empty from "./Empty.js";
 import useVisualMode from "../../hooks/useVisualMode";
-import Form from './Form.js';
-import Status from './Status.js'
-import Confirm from './Confirm.js'
-import Error from './Error.js'
+import Form from "./Form.js";
+import Status from "./Status.js";
+import Confirm from "./Confirm.js";
+import Error from "./Error.js";
 
 const EMPTY = "EMPTY";
 const SHOW = "SHOW";
-const CREATE = "CREATE"
-const SAVING = "SAVING"
-const DELETING = "DELETING"
-const CONFIRM = "CONFIRM"
-const EDIT = "EDIT"
-const ERROR_SAVE = "ERROR SAVING"
-const ERROR_DELETE = "ERROR DELETING"
+const CREATE = "CREATE";
+const SAVING = "SAVING";
+const DELETING = "DELETING";
+const CONFIRM = "CONFIRM";
+const EDIT = "EDIT";
+const ERROR_SAVE = "ERROR SAVING";
+const ERROR_DELETE = "ERROR DELETING";
 
 export default function Appointment(props) {
   const { mode, transition, back } = useVisualMode(
@@ -24,18 +24,18 @@ export default function Appointment(props) {
   );
 
   function destroy() {
-    transition(DELETING,true)
+    transition(DELETING, true);
     props
       .cancelInterview(props.id)
       .then(() => transition(EMPTY))
-      .catch((err) => transition(ERROR_DELETE, true))
+      .catch(err => transition(ERROR_DELETE, true));
   }
 
   function save(name, interviewer) {
     if (!name | !interviewer) {
-      transition(ERROR_SAVE, true)
+      transition(ERROR_SAVE, true);
     } else {
-      transition(SAVING)
+      transition(SAVING);
       const interview = {
         student: name,
         interviewer
@@ -43,68 +43,65 @@ export default function Appointment(props) {
       props
         .bookInterview(props.id, interview)
         .then(() => transition(SHOW))
-        .catch((err) => transition(ERROR_SAVE, true))
+        .catch(err => transition(ERROR_SAVE, true));
     }
   }
 
   return (
-  <article className="appointment">
-    <header>{props.time}</header>
-    {mode === EMPTY && <Empty onAdd={()=> {transition(CREATE)}} />}
-    {mode === SHOW && (
-    <Show
-    student={props.interview.student}
-    interviewer={props.interview.interviewer}
-    onDelete={()=>{transition(CONFIRM)}}
-    onEdit={()=>{
-      transition(EDIT)}}
-    />
-    )}
-    {mode === CREATE && (
-    <Form
-    interviewers={props.interviewers}
-    onSave={(name, interviewer) => save(name, interviewer)}
-    onCancel={()=>{transition(EMPTY)}} 
-    />
-    )}
-    {mode === SAVING && (
-      <Status
-      message={SAVING}
-      />
-    )}
-    {mode === DELETING && (
-      <Status
-      message={DELETING}
-      />
-    )}
-    {mode === CONFIRM && (
-      <Confirm
-      onConfirm={destroy}
-      onCancel={back}
-      />
-    )}
-    {mode === EDIT && (
-      <Form
-      interviewers={props.interviewers}
-      interviewer={props.interview.interviewer.id}
-      name={props.interview.student}
-      onSave={save}
-      onCancel={back}
-      />
-    )}
-    {mode === ERROR_SAVE && (
-      <Error
-      message={"There was an error saving the appointment!"}
-      onClose={back}
-      />
-    )}
-    {mode === ERROR_DELETE && (
-      <Error
-      message={"There was an error deleting the appointment!"}
-      onClose={back}
-      />
-    )}
-  </article>
-  
-  )
+    <article className="appointment">
+      <header>{props.time}</header>
+      {mode === EMPTY && (
+        <Empty
+          onAdd={() => {
+            transition(CREATE);
+          }}
+        />
+      )}
+      {mode === SHOW && (
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+          onDelete={() => {
+            transition(CONFIRM);
+          }}
+          onEdit={() => {
+            transition(EDIT);
+          }}
+        />
+      )}
+      {mode === CREATE && (
+        <Form
+          interviewers={props.interviewers}
+          onSave={(name, interviewer) => save(name, interviewer)}
+          onCancel={() => {
+            transition(EMPTY);
+          }}
+        />
+      )}
+      {mode === SAVING && <Status message={SAVING} />}
+      {mode === DELETING && <Status message={DELETING} />}
+      {mode === CONFIRM && <Confirm onConfirm={destroy} onCancel={back} />}
+      {mode === EDIT && (
+        <Form
+          interviewers={props.interviewers}
+          interviewer={props.interview.interviewer.id}
+          name={props.interview.student}
+          onSave={save}
+          onCancel={back}
+        />
+      )}
+      {mode === ERROR_SAVE && (
+        <Error
+          message={"There was an error saving the appointment!"}
+          onClose={back}
+        />
+      )}
+      {mode === ERROR_DELETE && (
+        <Error
+          message={"There was an error deleting the appointment!"}
+          onClose={back}
+        />
+      )}
+    </article>
+  );
 }
--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -1,22 +1,20 @@
 import React from "react";
 
-import './InterviewerListItem.scss'
-import classNames from 'classnames';
-
+import "./InterviewerListItem.scss";
+import classNames from "classnames";
 
 export default function DayListItem(props) {
   const itemClass = classNames("interviewers__item", {
-    "interviewers__item--selected": props.selected,
- })
-return (
-  <li className={itemClass} onClick={props.setInterviewer}>
-    <img
-      className="interviewers__item-image"
-      src={props.avatar}
-      alt={props.name}
-    />
-    {props.selected && props.name}
-  </li>
-);
+    "interviewers__item--selected": props.selected
+  });
+  return (
+    <li className={itemClass} onClick={props.setInterviewer}>
+      <img
+        className="interviewers__item-image"
+        src={props.avatar}
+        alt={props.name}
+      />
+      {props.selected && props.name}
+    </li>
+  );
 }
-
