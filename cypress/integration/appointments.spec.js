e22459e9a3dd069de4d882c1ed8b31b083f51dee
Emily <emily.hillier@gmail.com>
2019-11-05 16:25:40 -0800

    added cypress tests for booking, cancelling, and editing appointments

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -1,36 +1,67 @@
+import { format } from "path";
+
 describe("Appointments", () => {
+  beforeEach(() => {
+    cy.request("GET", "/api/debug/reset");
+  
+    cy.visit("/");
+  
+    cy.contains("Monday");
+   });
+
   it("should book an interview", () => {
-  // Visits the root of our web server
-  cy.visit("/")
-  cy.contains("Monday")
-  // Clicks on the "Add" button in the second appointment
-  cy.get("[alt=Add]")
-  .first()
-  .click();
-  // Enters their name
-  cy.get("[data-testid=student-name-input]")
-    .type("Lydia Miller-Jones")
-  // Chooses an interviewer
-
-  // Clicks the save button
-
-  // Sees the booked appointment
-
+    // Clicks on the "Add" button in the second appointment
+    cy.get("[alt=Add]")
+    .first()
+    .click();
+    // Enters their name
+    cy.get("[data-testid=student-name-input]")
+      .type("Lydia Miller-Jones")
+    // Chooses an interviewer
+    cy.get("[alt='Sylvia Palmer']").click()
+    // Clicks the save button
+    cy.contains("Save").click()
+    // Sees the booked appointment
+    cy.contains(".appointment__card--show", "Lydia Miller-Jones");
+    cy.contains(".appointment__card--show", "Sylvia Palmer");
   })
 
 
 // Editing
-// Visits the root of our web server
-// Clicks the edit button for the existing appointment (Archie Cohen)
-// Changes the name and interviewer
-// Clicks the save button
-// Sees the edit to the appointment
+it("should edit an interview", () => {
+  // Clicks the edit button for the existing appointment (Archie Cohen)
+  cy.get("[alt=Edit]")
+    .first()
+    .click({ force: true });
+  // Changes the name and interviewer
+  cy.get("[data-testid=student-name-input]")
+    .clear()
+    .type("Lydia Miller-Jones")
 
+  cy.get("[alt='Tori Malcolm']")
+    .click()
+  // Clicks the save button
+  cy.contains("Save")
+    .click()
+  // Sees the edit to the appointment
+  cy.contains(".appointment__card--show", "Lydia Miller-Jones");
+  cy.contains(".appointment__card--show", "Tori Malcolm");
+})
 
 // Canceling
-// Visits the root of our web server
-// Clicks the delete button for the existing appointment
-// Clicks the confirm button
-// Sees that the appointment slot is empty
-
+it("should cancel an interview", () => {
+  // Clicks the delete button for the existing appointment
+  cy.get("[alt=Delete]")
+    .first()
+    .click({ force: true });
+  // Clicks the confirm button
+  cy.contains("Confirm")
+    .click()
+  // Sees that the appointment slot is empty
+  cy.contains("DELETING").should("exist");
+  cy.contains("DELETING").should("not.exist");
+  cy.contains(".appointment__card--show", "Archie Cohen")
+    .should("not.exist");
+  
+})
 })
